ae11adf6ccbf85038a17fcce1cf01772be981343
matteo <matteo.cirio98@gmail.com>
2025-01-14 11:58:30 +0100

    Try 3 tests

--- SumTest/SumTest.cs
+++ SumTest/SumTest.cs
@@ -4,7 +4,7 @@ namespace SumTest
     public class SumTest
     {
         [Fact]
-        public void TestSomma()
+        public void TestSomma1()
         {
             var sum = new Sum();
             int a = 18;
@@ -12,6 +12,28 @@ namespace SumTest
             int result = sum.Somma(a, b);
 
             Assert.Equal(33, result);
+        }
+
+        [Fact]
+        public void TestSomma2()
+        {
+            var sum = new Sum();
+            int a = 20;
+            int b = 15;
+            int result = sum.Somma(a, b);
+
+            Assert.Equal(35, result);
+        }
+
+        [Fact]
+        public void TestSomma3()
+        {
+            var sum = new Sum();
+            int a = 20;
+            int b = 20;
+            int result = sum.Somma(a, b);
+
+            Assert.Equal(40, result);
         }
     }
 }
